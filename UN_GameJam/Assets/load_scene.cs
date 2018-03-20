d9e12898585608a0fdfdd67a3183d1ddce3f337d
Tom Coldenhoff <tom.coldenhoff@outlook.com>
2018-03-20 15:27:04 +0100

    Removed Libary tracking

---
+++ UN_GameJam/Assets/load_scene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class load_scene : MonoBehaviour
+{
+
+    public void Load_scene_Main()
+    {
+        SceneManager.LoadScene("GenScene");
+    }
+    public void Load_scene_Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+}
