a0b0f920faf2b7e144923941ccc723decb60e5e3
Oliver <Powerking2000@gmail.com>
2018-03-22 13:32:40 +0100

    animals now appear on the map, "waddling"

---
+++ UN_GameJam/Assets/Scripts/Animal_movement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal_movement : MonoBehaviour {
+    Vector2 current_pos;
+    private Rigidbody2D rb;
+
+    private float timeLeft;
+    private Vector3 direction;
+    private float speed = 3;
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        timeLeft = Random.Range(2, 5);
+        rb = GetComponent<Rigidbody2D>();
+        current_pos = new Vector2(Random.Range(10, 100), Random.Range(10, 100));
+        ChangeDirection();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        WaitForNewDirection();
+        Move();
+
+	}
+
+    private void WaitForNewDirection()
+    {
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft <= 0)
+        {
+            ChangeDirection();
+            timeLeft = Random.Range(2, 5);
+        }
+    }
+
+    private void ChangeDirection()
+    {
+        Vector3 tempDirection = (Random.insideUnitCircle * 3) + new Vector2(this.transform.position.x, this.transform.position.y);
+        tempDirection -= this.transform.position;
+        tempDirection.Normalize();
+
+        direction = tempDirection;
+    }
+
+    private void Move()
+    {
+        this.transform.Translate((direction * speed) * Time.deltaTime);
+    }
+}
--- UN_GameJam/Assets/Scripts/animal_controller.cs
+++ UN_GameJam/Assets/Scripts/animal_controller.cs
@@ -2,15 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class animal_controller : MonoBehaviour {
+public class animal_controller : MonoBehaviour
+{
+    public Rigidbody2D player;
+    public float x_length = 10f;
+    public float y_length = 10f;
+    public int amount = 10;
+    private Vector3 new_position;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    [SerializeField] GameObject[] animals;
+    
+    GameObject temp_animal;
+    bool can_spawn = true;
+    // Use this for initialization
+    void Start()
+    {
+        spawn();
+        
+    }
+ 
+    void spawn()
+    {
+        if (amount > 0)
+        {
+            temp_animal = animals[Random.Range(0, animals.Length)];
+            new_position = new Vector3(Random.Range(-150, 150), Random.Range(-50, 50), 0);
+            Instantiate(temp_animal, player.transform.position + new_position, Quaternion.identity);
+            amount -= 1;
+            spawn();
+        }
+        
+            
+    }
+
+    private void Update()
+    {
+        if (amount == 0)
+        {
+            spawn();
+        }
+    }
 }
+    
+
