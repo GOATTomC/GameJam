e57a73d9b1fb898b2f56baf241f453287eea5d89
Tom Coldenhoff <tom.coldenhoff@outlook.com>
2018-03-22 10:04:54 +0100

    Timer

--- UN_GameJam/Assets/Scripts/Timer.cs
+++ UN_GameJam/Assets/Scripts/Timer.cs
@@ -8,11 +8,17 @@ public class Timer : MonoBehaviour {
 
     private float m_TimeLeftSeconds = 30;
     [SerializeField] private Text m_TimerText;
+    [SerializeField] private AudioSource m_TickAudio;
+    [SerializeField] private AudioClip m_Tick;
+    [SerializeField] private AudioClip m_Tock;
     private bool m_IsZero = false;
+    private bool m_IsEven;
+    private bool m_Previous = false;
+    private Color m_NormalColor;
 
 	// Use this for initialization
 	void Start () {
-		
+        m_NormalColor = m_TimerText.color;
 	}
 	
 	// Update is called once per frame
@@ -26,6 +32,20 @@ public class Timer : MonoBehaviour {
 
         if (!m_IsZero)
             m_TimeLeftSeconds -= Time.deltaTime;
+
+        m_IsEven = Mathf.Floor(m_TimeLeftSeconds % 2) > 0 ? false : true;
+
+        if (m_IsEven != m_Previous)
+        {
+            if (m_TimeLeftSeconds <= 10)
+            {
+                m_TickAudio.clip = m_IsEven == true ? m_Tock : m_Tick;
+                m_TickAudio.Play();
+                m_Previous = m_IsEven;
+            }
+        }
+
+        m_TimerText.color = m_IsEven == true ? Color.red : m_NormalColor;
 
         float minutes = Mathf.Floor(m_TimeLeftSeconds / 60);
         float seconds = Mathf.RoundToInt(m_TimeLeftSeconds % 60);
